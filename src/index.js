fb34d9bb5a35c3309c817ed9a8dda473cc7636e6
Sean Yesmunt <sean.yesmunt@gmail.com>
2017-03-18 21:10:08 -0700

    init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,62 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+require('./style.scss')
 
-const App = () => (
-  <div>Hello world</div>
-)
+function portals() {
+  document.addEventListener('keydown', _keyDownHandler)
 
-ReactDOM.render(React.createElement(App), document.getElementById('js-content'))
+  const mover = document.querySelector('.mover')
+  const portals = document.querySelector('portal')
+  const blue = document.querySelector('.blue')
+  const orange = document.querySelector('.orange')
+
+  blue.addEventListener('mousedown', () => togglePortalMovement('movingBluePortal'))
+  orange.addEventListener('mousedown', () => togglePortalMovement('movingOrangePortal'))
+  blue.addEventListener('mouseup', () => togglePortalMovement('movingBluePortal'))
+  orange.addEventListener('mouseup', () => togglePortalMovement('movingOrangePortal'))
+
+
+  document.addEventListener('mousemove', _handleMouseMove)
+
+  const state = {
+    location: {
+      top: 0,
+      left: 0
+    },
+    movingBluePortal: false,
+    movingOrangePortal: false
+  }
+
+  function togglePortalMovement (movingColorPortal) {
+    state[movingColorPortal] = !state[movingColorPortal];
+  }
+
+  function _handleMouseMove (ev) {
+    if (state.movingOrangePortal || state.movingBluePortal) {
+      console.log('ev', ev);
+    }
+  }
+
+  function _keyDownHandler (ev) {
+    switch (ev.key) {
+      case 'ArrowUp':
+        updateState('top', -5)
+        break
+      case 'ArrowRight':
+        updateState('left', 5)
+        break
+      case 'ArrowDown':
+        updateState('top', 5)
+        break
+      case 'ArrowLeft':
+        updateState('left', -5)
+        break
+    }
+  }
+
+  function updateState (direction, val) {
+    const newMargin = state.location[direction] + val
+    state.location[direction] = newMargin
+    mover.style[direction] = newMargin
+  }
+}
+
+portals()
--- webpack.config.js
+++ webpack.config.js
@@ -6,17 +6,40 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-
   module: {
-    loaders: [
+    rules: [
       {
+        test: /\.js$/, // include .js files
+        enforce: 'pre', // preload the jshint loader
+        exclude: /node_modules/, // exclude any and all files in the node_modules folder
+        use: [
+            {
+                loader: 'jshint-loader',
+                options: {
+                  asi: true,
+                  esversion: 6,
+                  quotmark: 'single'
+                }
+            }
+        ]
+      },
+      {
+        test: /\.js$/,
         loader: 'babel-loader',
+        exclude: /node_modules/,
         include: [
           path.resolve(__dirname, 'src')
         ],
         query: {
-          presets: ['es2015', 'stage-0', 'react']
+          presets: ['es2015', 'stage-0']
         }
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!sass-loader',
+        include: [
+          path.resolve(__dirname, 'src')
+        ]
       }
     ]
   }
